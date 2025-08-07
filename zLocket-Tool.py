bbd221563e2535ebdf8ddbe7b25e0eb02c0c20ef
haianhbinq1-spec <haianhbinq1@gmail.com>
2025-08-07 21:26:36 +0700

    Locket by Haianh

--- zLocket-Tool.py
+++ zLocket-Tool.py
@@ -1,9 +1,9 @@
 # ==================================
 #!/usr/bin/env python
 # coding: utf-8
-# Telegram: @wus_team
+# Facebook:haianh
 # Version: 1.0.7
-# Github: https://github.com/wusthanhdieu
+# Github: https://github.com/haianhbinq1
 # Description: zLocket Tool Open Source
 # ==================================
 import sys
